8e06511fcf13ec8a815b719f48d44a570e84d53e
Andrew <abogdanov@altsoft.biz>
2015-12-02 01:05:29 +0300

    Project ready.

--- src/parking/Parking.java
+++ src/parking/Parking.java
@@ -14,9 +14,11 @@ import java.util.Map;
  */
 public class Parking {
 
-    Map<Integer, Vehicle> parking;
-    int capacity;
-    float price;
+    private final int NIGHT_HOURS = 7;
+    private final int HOURS_IN_DAY = 24;
+    private final Map<Integer, Vehicle> parking;
+    private final int capacity;
+    private final float price;
 
     public Parking(int aCapacity, float aPrice) {
         if (aCapacity >= 0) {
@@ -42,7 +44,21 @@ public class Parking {
     public float unPark(int aID, int aTime) {
         Vehicle vehicle = parking.remove(aID);
         if (vehicle != null) {
-            int hours = aTime - vehicle.getTime();
+            int nightHours;
+            int firstDayTime = HOURS_IN_DAY - vehicle.getTime();
+            if (firstDayTime < aTime) {
+                int hours = aTime - (firstDayTime < HOURS_IN_DAY ? firstDayTime : 0);
+                int lastDayTime = hours % HOURS_IN_DAY;
+                int days = hours / HOURS_IN_DAY;
+                nightHours = days * NIGHT_HOURS + (lastDayTime < 6 ? lastDayTime : 6);
+                if (firstDayTime < HOURS_IN_DAY) {
+                    nightHours += (vehicle.getTime() < 6 ? 6 - vehicle.getTime() : 0) + 1;
+                }
+            } else {
+                nightHours = (vehicle.getTime() + aTime) <= 6 ? aTime : 6 - vehicle.getTime();
+                nightHours += (vehicle.getTime() + aTime) == HOURS_IN_DAY ? 1 : 0;
+            }
+            return nightHours * price * 2 + (aTime - nightHours) * price;
         }
         return 0f;
     }
--- src/parking/ParkingLife.java
+++ src/parking/ParkingLife.java
@@ -5,6 +5,10 @@
  */
 package parking;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author Andrew
@@ -15,7 +19,51 @@ public class ParkingLife {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int iterations = 0;
+        int capacity = -1;
+        float price = 0f;
+        for (int i = 0; i < args.length; i++) {
+            if ("-iterations".equals(args[i])) {
+                if (i + 1 < args.length) {
+                    iterations = Integer.parseInt(args[i + 1]);
+                } else {
+                    System.err.println("Iterations count not specified.");
+                    System.exit(1);
+                }
+            } else if ("-places".equals(args[i])) {
+                if (i + 1 < args.length) {
+                    capacity = Integer.parseInt(args[i + 1]);
+                } else {
+                    System.err.println("Vehicle places count not specified.");
+                }
+            } else if ("-price".equals(args[i])) {
+                if (i + 1 < args.length) {
+                    price = Float.parseFloat(args[i + 1]);
+                } else {
+                    System.err.println("One hour price not specified.");
+                }
+            } 
+        }
+        List<Integer> parkedVehicles = new ArrayList<>(10);
+        Parking parking = new Parking(capacity, price);
+        Random rnd = new Random();
+        for (int i = 0; i < iterations; i++) {
+            if (i % 10 == 0 && i > 0) {
+                int unparkVehicles = rnd.nextInt(10);
+                for (int j = 0; j < unparkVehicles; j++) {
+                    int id = parkedVehicles.get(j);
+                    float purchase = parking.unPark(id, rnd.nextInt(100));
+                    System.out.println("Vehicle " + String.valueOf(id) + " successfully unparked. The cost amounted to " + String.valueOf(purchase) + " rub.");
+                }
+            }
+            int id = rnd.nextInt(10000);
+            if (parking.park(id, rnd.nextInt(25))) {
+                System.out.println("Vehicle " + String.valueOf(id) + " successfully parked.");
+                parkedVehicles.add(id);
+            } else {
+                System.out.println("Vehicle " + String.valueOf(id) + " could not be parked. Parking has not free places.");
+            }
+        }
     }
-    
+
 }
